08d01d1b793fad704adac0adfdf155cbc4d8c641
Nahuel Alberti <NahuelAlbertiSalatino@gmail.com>
2019-05-17 01:26:55 -0300

    Solucionados problemas css

---
+++ precache-manifest.ea8f951cafd60fedf9d97db64447ca82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2689ee0d85b31e8dc3816bde82fa7c6f",
+    "url": "/TheMovieDBApp/index.html"
+  },
+  {
+    "revision": "aea3f69415d7eab65c6c",
+    "url": "/TheMovieDBApp/static/css/main.44a36be5.chunk.css"
+  },
+  {
+    "revision": "a56843670e5c025c5871",
+    "url": "/TheMovieDBApp/static/js/2.3e191eea.chunk.js"
+  },
+  {
+    "revision": "aea3f69415d7eab65c6c",
+    "url": "/TheMovieDBApp/static/js/main.9883f4de.chunk.js"
+  },
+  {
+    "revision": "14faef2e50f126f38786",
+    "url": "/TheMovieDBApp/static/js/runtime~main.1988ae7d.js"
+  }
+]);
