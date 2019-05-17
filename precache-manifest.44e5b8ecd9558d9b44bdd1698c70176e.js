70dc109e173641f0ab20d4ee51473d162bcc1e41
Nahuel Alberti <NahuelAlbertiSalatino@gmail.com>
2019-05-17 01:21:18 -0300

    Solucionado problemas del responsive

---
+++ precache-manifest.44e5b8ecd9558d9b44bdd1698c70176e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd31a82c7e9a7164c290063f47d227b5",
+    "url": "/TheMovieDBApp/index.html"
+  },
+  {
+    "revision": "78ee173cea6d3650467e",
+    "url": "/TheMovieDBApp/static/css/main.261cc8e0.chunk.css"
+  },
+  {
+    "revision": "a56843670e5c025c5871",
+    "url": "/TheMovieDBApp/static/js/2.3e191eea.chunk.js"
+  },
+  {
+    "revision": "78ee173cea6d3650467e",
+    "url": "/TheMovieDBApp/static/js/main.9883f4de.chunk.js"
+  },
+  {
+    "revision": "14faef2e50f126f38786",
+    "url": "/TheMovieDBApp/static/js/runtime~main.1988ae7d.js"
+  }
+]);
